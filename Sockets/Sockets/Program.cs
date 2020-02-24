861b6bd644e460a99dccb29e3201bdf24229b3b2
Olya <olga.akinshyna@gmail.com>
2020-02-24 20:19:04 +0200

    sln added

---
+++ Sockets/Sockets/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here will be socket opened!!!");
+        }
+    }
+}
